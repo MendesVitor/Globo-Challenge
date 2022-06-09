7bac5a84b0dda41510faa9df9ad8c89141e60163
Vitor Mendes <vitor.mendes93@gmail.com>
2022-06-08 21:41:50 -0300

    api: setup env vars

--- rest-api/src/main.ts
+++ rest-api/src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { validateEnvVars } from './shared/configs/environment.config';
 
 async function bootstrap() {
+  validateEnvVars();
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000);
