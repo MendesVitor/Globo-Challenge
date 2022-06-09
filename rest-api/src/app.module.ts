a00ebb7d105a2e315150b946b37f9f36400ab2e2
Vitor Mendes <vitor.mendes93@gmail.com>
2022-06-08 21:42:19 -0300

    api: connecting to db using TypeORM

--- rest-api/src/app.module.ts
+++ rest-api/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CardsModule } from './modules/cards/cards.module';
 import { TagsModule } from './modules/tags/tags.module';
+import { databaseConfig } from './shared/configs/database.config';
 
 @Module({
-  imports: [CardsModule, TagsModule],
+  imports: [TypeOrmModule.forRoot(databaseConfig()), CardsModule, TagsModule],
 })
 export class AppModule {}
