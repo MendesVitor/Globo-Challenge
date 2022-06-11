a389ffe0b2cefc1d716304ccec56c9bef6054ea8
Vitor Mendes <vitor.mendes93@gmail.com>
2022-06-11 18:35:55 -0300

    cli: parsing data from csv and storing in database

--- cardimportcli/src/commands/import/index.ts
+++ cardimportcli/src/commands/import/index.ts
@@ -1,34 +1,87 @@
-import {Command} from '@oclif/core'
-import {client} from '../../database'
+import { Command } from "@oclif/core";
+import { readFile } from "node:fs/promises";
+import { join, resolve } from "node:path";
+import { cwd } from "node:process";
+import { client } from "../../database";
+import { parse } from "csv-parse";
+import { createReadStream } from "node:fs";
 
 export default class Import extends Command {
-  static description = 'Import card from CSV to database';
+  static description = "Import card from CSV file to database";
 
-  static examples = [''];
-
-  // static flags = {
-  //   from: Flags.string({char: 'f', description: 'Whom is saying hello', required: true}),
-  // }
+  static examples = ["card import C:\\Users\\user\\Desktop\\test.csv"];
 
   static args = [
     {
-      name: 'CSV file path',
-      description: 'path of the CSV file',
+      name: "path",
+      description: "absolute path of the CSV file",
       required: true,
     },
   ];
 
   async run(): Promise<void> {
-    const {args, flags} = await this.parse(Import)
+    const { args } = await this.parse(Import);
 
-    await client.connect()
+    const parser = parse(async (_, data) => {
+      if (data.length < 1) {
+        this.log("there is nothing to import");
+        return;
+      }
 
-    const res = await client.query('SELECT * from card')
-    await client.end()
-    this.log(res.rows[0])
+      let tagsResponse;
+      const conn = await readFile(join(cwd(), "conn.txt"), {
+        encoding: "utf8",
+      });
 
-    this.log(
-      `hello ${args.person} from ${flags.from}! (./src/commands/hello/index.ts)`,
-    )
+      const db = client(conn);
+
+      await db.connect();
+      try {
+        await db.query("BEGIN");
+
+        for (let i = 1; i < data.length; i++) {
+          if (data[i][0] && data[i][1]) {
+            const cardsResponse = await db.query(
+              `INSERT INTO card(text) VALUES('${data[i][0]}') RETURNING id`
+            );
+
+            for (const element of data[i][1].split(";")) {
+              const tag = await db.query(
+                `SELECT id FROM tag WHERE name = '${element}'`
+              );
+              if (tag.rowCount < 1) {
+                tagsResponse = await db.query(
+                  `INSERT INTO tag(name) VALUES('${element}') RETURNING id`
+                );
+              }
+
+              await db.query(
+                `INSERT INTO card_tags_tag("cardId","tagId") VALUES('${
+                  cardsResponse.rows[0].id
+                }','${tagsResponse?.rows[0].id ?? tag.rows[0].id}')`
+              );
+            }
+          }
+        }
+
+        await db.query("COMMIT");
+      } catch (error) {
+        await db.query("ROLLBACK");
+
+        this.log((error as Error).message);
+      } finally {
+        db.end();
+        this.log("data imported");
+      }
+    });
+
+    createReadStream(resolve(args.path))
+      .on("error", (error) => {
+        this.log((error as Error).message);
+      })
+      .pipe(parser)
+      .on("error", (error) => {
+        this.log((error as Error).message);
+      });
   }
 }
